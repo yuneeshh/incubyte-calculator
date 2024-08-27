2e495665ba73f43670cd23439083dfb250a493b2
Yuneesh Uddeshiya <yuneeshh@gmail.com>
2024-08-27 12:03:49 +0530

    Add test for single digit

--- calculator.py
+++ calculator.py
@@ -1,15 +1,18 @@
 import unittest
 
 
-def calculator(numbers: str) ->int:
-    return 0
+def calculator(numbers: str) -> int:
+    numbers = numbers.strip()
+    return sum(int(number) for number in numbers.split())
 
 
 class TestCalculator(unittest.TestCase):
-
-    def test_add_empty(self):
+    def test_calculator_empty_string(self):
         self.assertEqual(calculator(""), 0)
 
+    def test_calculator_single_digit_string(self):
+        self.assertEqual(calculator("5"), 5)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
