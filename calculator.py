85877b6403c9f5fe66a616b538922a8797967d7c
Yuneesh Uddeshiya <yuneeshh@gmail.com>
2024-08-27 12:07:47 +0530

    Add more test for single digit sum

--- calculator.py
+++ calculator.py
@@ -1,18 +1,27 @@
 import unittest
 
 
-def calculator(numbers: str) -> int:
+def calculator(numbers: str) ->int:
     numbers = numbers.strip()
     return sum(int(number) for number in numbers.split())
 
 
 class TestCalculator(unittest.TestCase):
+
     def test_calculator_empty_string(self):
         self.assertEqual(calculator(""), 0)
 
     def test_calculator_single_digit_string(self):
         self.assertEqual(calculator("5"), 5)
 
+    def test_calculator_invalid_digit(self):
+        with self.assertRaises(ValueError):
+            calculator("a")
 
-if __name__ == "__main__":
+    def test_calculator_multiple_invalid_digit(self):
+        with self.assertRaises(ValueError):
+            calculator(",x,k,,")
+
+
+if __name__ == '__main__':
     unittest.main()
